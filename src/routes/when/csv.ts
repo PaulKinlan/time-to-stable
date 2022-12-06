e47be960b2cfec312ceb9fd9834aaab278855207
Paul Kinlan <paul.kinlan@gmail.com>
2022-12-06 13:50:47 +0000

    csv only outputs filtered data

--- src/routes/when/csv.ts
+++ src/routes/when/csv.ts
@@ -3,21 +3,24 @@ import { CompatResult } from "../../types.d.ts";
 import { WhenRender } from "../types.d.ts";
 
 const renderBrowserTimes = (featureConfig, feature: CompatResult) => {
-  return Object.values(feature.all).map(({name, version_added, date_added, version_removed, date_removed}) => { 
-    return `${feature.api}|${featureConfig[feature.category].name}|${name}|${date_added || ""}|${version_added|| ""}|${date_removed || ""}|${version_removed || ""}`;
+  return Object.values(feature.stable).map(({ name, version_added, date_added, version_removed, date_removed }) => {
+    return `${feature.api}|${featureConfig[feature.category].name}|${name}|${date_added || ""}|${version_added || ""}|${date_removed || ""}|${version_removed || ""}`;
   }).join('\n')
 };
 
 export default function render({ bcd, features, browsers, browserList, selectedBrowsers, selectedFeatures, helper, all, featureConfig, warnings }: WhenRender): Response {
   const { __meta } = bcd
 
-  const header = template`API|Category|Browser|Release Date|Release Version|Removed Date|Removed Version\n`;
+  const header = template`API|Category|First Browser|Release Date|Last Browser|Release Date\n`;
 
-  const results = template`${features.map(feature =>
-    template`${renderBrowserTimes(featureConfig, feature)}\n`
-  )}`
+  const results = template`${features.map(feature => {
+    const { all, stableStats } = feature;
+    const { first, last } = stableStats;
 
-  return template`${header}${results}`.then(data => new Response(data, {
+    return template`${feature.api}|${featureConfig[feature.category].name}|${all[first.browser].name}|${first.added.toLocaleDateString()}|${all[first.browser].name}|${last.added.toLocaleDateString()}\n`
+  })}`
+
+  return template`${header}${results} `.then(data => new Response(data, {
     status: 200,
     headers: { "content-type": "text/plain; charset=utf-8" }
   }));
