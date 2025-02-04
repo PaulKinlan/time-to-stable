60b508eb5931542be46d8f3685444bc87bb11a96
Paul Kinlan <paul.kinlan@gmail.com>
2025-02-04 10:58:32 +0000

    Updates required for deno deploy timed build

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ import { contentType } from "https://deno.land/std@0.152.0/media_types/mod.ts";
 
 // @deno-types="https://esm.sh/@mdn/browser-compat-data@latest/types.d.ts"
 //import bcd from "https://esm.sh/@mdn/browser-compat-data@latest";
-import bcd from 'https://unpkg.com/@mdn/browser-compat-data' assert { type: 'json' };
+import bcd from 'https://unpkg.com/@mdn/browser-compat-data' with { type: 'json' };
 
 
 import index from "./src/routes/index.ts";
